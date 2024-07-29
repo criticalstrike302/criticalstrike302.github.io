a71688d169a82e46f03beb2edf99bf86d58c8f8e
Concrete <criticalstrike302@gmail.com>
2024-07-28 20:06:04 -0700

    minor

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": [],
         "url": "/Intro/",
         "teaser": null
+      },{
+        "title": "Family",
+        "excerpt":"Welcome to this week’s edition of “Dr.’s Note”. I’m Dr. J.J. James. Today we will dive into the conditions inmates face while incarcerated dealing with family. Our guest will be a 40 year old man we will call “ C.J.” Here is his story.- I grew up in Laredo. I...","categories": [],
+        "tags": [],
+        "url": "/Family/",
+        "teaser": null
       }]
